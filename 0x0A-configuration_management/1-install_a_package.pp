685fcfc64b366846466475857eebb9e3a7095ea5
Doctorstrange <Doctorstrange>
2023-11-24 14:34:12 -0600

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,7 @@
-# create a file in /tmp. using puppet
+#!/usr/bin/pup
+# install flask version 2.1.0
 
-file {'/tmp/school':
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-  content => 'I love Puppet',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
