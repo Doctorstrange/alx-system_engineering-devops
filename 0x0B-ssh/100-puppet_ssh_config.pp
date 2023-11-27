8085a0ff1f7adb810293cef1c9c875d93942f3ac
Doctorstrange <Doctorstrange>
2023-11-26 21:02:38 -0600

    puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# puppet connect without password
+
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
+
+file_line { 'Turn pff passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+  match => '^#PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#IdentityFile',
+}
