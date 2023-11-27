070d5455b5c0f4117f05124b6be5ed94f619e6e3
Doctorstrange <Doctorstrange>
2023-11-26 21:05:26 -0600

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,13 +5,13 @@ file { '/etc/ssh/ssh_config':
   ensure => present,
 }
 
-file_line { 'Turn pff passwd auth':
+file_line { 'passwd auth off':
   path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
   match => '^#PasswordAuthentication',
 }
 
-file_line { 'Declare identity file':
+file_line { 'Declare file identity':
   path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
   match => '^#IdentityFile',
