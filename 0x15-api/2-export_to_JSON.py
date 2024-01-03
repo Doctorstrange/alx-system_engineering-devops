4bac95b0f65ca6d83dcb7c87b6797d6f5c494b44
Doctorstrange <Doctorstrange>
2024-01-02 19:38:47 -0600

    python3

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Accessing todo lists of employees"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    empId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + empId
+
+    response = requests.get(url)
+    empuser = response.json().get('username')
+
+    toUrl = url + "/todos"
+    response = requests.get(toUrl)
+    tasks = response.json()
+
+    dic = {empId: []}
+    for task in tasks:
+        dic[empId].append({
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": empuser
+        })
+    with open('{}.json'.format(empId), 'w') as f:
+        json.dump(dic, f)
