fef5503c08ad3eb931fc2e13c8a8b7cce20b74de
Doctorstrange <doctorstrange247@gmail.com>
2024-01-16 22:30:15 +0000

    1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" queries the Reddit API and prints the titles of the first 10 hot posts """
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """ queries the Reddit API and prints the titles of the first 10 hot posts """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    user_agent = {'User-agent': 'Google Chrome Version 120.0.6099.216'}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params={'limit': 10})
+    injson = response.json()
+
+    try:
+        result = injson.get('data').get('children')
+
+        for obj in result:
+            print(obj.get('data').get('title'))
+
+    except Exception:
+        print("None")
