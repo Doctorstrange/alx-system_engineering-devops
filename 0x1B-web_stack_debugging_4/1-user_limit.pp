f116ea37119f1951724e01404355f76113114138
Doctorstrange <doctorstrange247@gmail.com>
2024-01-25 21:05:02 +0000

    hard-limit-increase-for-holberton

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+#enables user holberton to login
+
+# Increase limit for holberton
+exec { 'hard-limit-increase-for-holberton':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for holberton
+exec { 'soft-limit-increase-for-holberton':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
