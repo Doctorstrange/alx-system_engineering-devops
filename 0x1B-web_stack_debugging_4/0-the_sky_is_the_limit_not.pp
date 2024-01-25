81b13a064964bcff7a857ecb82d3f1c47ccbc795
Doctorstrange <doctorstrange247@gmail.com>
2024-01-25 20:52:34 +0000

    # increase nginx handle limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,12 @@
+# increase nginx handle limit
+
 # Increase the ULIMIT
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
+#restart nginx
 -> exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
